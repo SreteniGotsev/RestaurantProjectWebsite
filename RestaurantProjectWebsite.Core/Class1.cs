138ca5561ca69983a9f4894396f83d5a51520bb5
SreteniGotsev <sreteni.workmail@gmail.com>
2024-03-06 19:55:16 +0000

    Add project files.

---
+++ RestaurantProjectWebsite.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantProjectWebsite.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ RestaurantProjectWebsite.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantProjectWebsite.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
