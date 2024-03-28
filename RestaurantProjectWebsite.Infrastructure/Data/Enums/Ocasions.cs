619bb42958a560caf088f196f8b07600b33657d6
SreteniGotsev <sreteni.workmail@gmail.com>
2024-03-28 15:23:59 +0000

    Enums Added

---
+++ RestaurantProjectWebsite.Infrastructure/Data/Enums/Ocasions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProjectWebsite.Infrastructure.Data.Enums
+{
+    public enum Ocasions
+    {
+        Birthday =0,
+        Nameday =1,
+        Annivesary =2,
+    }
+}
---
+++ RestaurantProjectWebsite.Infrastructure/Data/Enums/SubProductType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProjectWebsite.Infrastructure.Data.Enums
+{
+    public enum SubProductType
+    {
+        Starter=0,
+        Main=1,
+        Dessert=2,
+        Cocktail=3,
+        Spirit=4,
+        Soft=5
+    }
+}
