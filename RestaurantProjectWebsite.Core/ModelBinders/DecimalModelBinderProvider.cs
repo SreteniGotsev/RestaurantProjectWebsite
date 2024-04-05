725071c0024dfe6d31270348779a49eb05ecd0b9
SreteniGotsev <sreteni.workmail@gmail.com>
2024-04-05 15:02:39 +0100

    Decimal Model Binder copied

---
+++ RestaurantProjectWebsite.Core/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProjectWebsite.Core.ModelBinders
+{
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+
+            if(context.Metadata.ModelType != typeof(decimal)
+                ||context.Metadata.ModelType == typeof(decimal?))
+            {
+                return new DecimalModelBinder();
+            }
+
+            return null;
+        }
+    }
+}
